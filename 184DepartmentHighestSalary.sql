d87846c7e0e2582ab46ea2c3b1cb76dae24dc5dc
Jamie Wang <46794277+jamielaiwang@users.noreply.github.com>
2020-01-03 09:34:02 -0600

    Update 184DepartmentHighestSalary.sql
    This is my solution of Leetcode database 184 Department Highest Salary

--- 184DepartmentHighestSalary.sql
+++ 184DepartmentHighestSalary.sql
@@ -1,3 +1,9 @@
+#First select maxmium salary in each department with corresponding Id. 
+#Then inner join the result table with Employee to get the Employee name 
+#who has maximum salary, DepartmentId and maximum salary. 
+#Then join the result table with the Department table to get the Department Name 
+#based on Department id.
+
 Select D.Name as Department, T.Name as Employee, T.Salary as Salary
 from Department as D, 
  (Select Name, Salary, E.DepartmentId from Employee E inner join
